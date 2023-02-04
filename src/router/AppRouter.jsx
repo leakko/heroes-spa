d63472635729e5cea1308f1e8bc266a01e8fe35c
Marcos Rodriguez Salvo <marcossalvo95@gmail.com>
2023-02-04 20:44:22 +0100

    Add active class to NavLink

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,19 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import { LoginPage } from "../auth/pages/LoginPage"
-import { DcPage } from "../heroes/pages/DCPage"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
+import { LoginPage } from "../auth/"
+import { DcPage, MarvelPage } from "../heroes"
+import { Navbar } from "../ui"
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path="marvel" element={ <MarvelPage /> }/>
-            <Route path="dc" element={ <DcPage /> } />
+      <Navbar />
+      <Routes>
+          <Route path="marvel" element={ <MarvelPage /> }/>
+          <Route path="dc" element={ <DcPage /> } />
 
-            <Route path="login" element={ <LoginPage /> } />
-            <Route path="/" element={ <Navigate to="/marvel" /> } />
-        </Routes>
+          <Route path="login" element={ <LoginPage /> } />
+          <Route path="/" element={ <Navigate to="/marvel" /> } />
+      </Routes>
     </>
   )
 }
